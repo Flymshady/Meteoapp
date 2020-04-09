1f3fbc6d7ddd820f04bc71555d74e050c9d363ad
Štěpán Cellar <stepan.cellar@gmail.com>
2020-04-09 17:47:36 +0200

    Firebasestore & Marker layout

--- app/src/main/java/cz/cellar/meteoapp/MapActivity.java
+++ app/src/main/java/cz/cellar/meteoapp/MapActivity.java
@@ -1,7 +1,9 @@
 package cz.cellar.meteoapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,11 +11,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapActivity  extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +46,26 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+     //   LatLng sydney = new LatLng(-34, 151);
+      //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setOnMapLongClickListener(this);
+
+        db.collection("markers")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            return;
+                        }
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                LatLng latLng = new LatLng(document.getDouble("lat"), document.getDouble("lng"));
+                                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+                        }
+                    }
+                });
+
     }
 
     @Override
@@ -41,7 +73,20 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
         // Add a marker on click
 
         mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-       // mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
+        Map<String, Object> user = new HashMap<>();
+        user.put("lat", latLng.latitude);
+        user.put("lng", latLng.longitude);
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("markers").add(user);
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        String d = marker.getId();
+        return false;
     }
 }
